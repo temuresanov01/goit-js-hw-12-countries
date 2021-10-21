a48613ea484cd08b8e166bf052ee4c580f02ec05
Temur Esanov <temuresanov01@gmail.com>
2021-10-21 18:52:41 +0200

    код и стили

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,29 +1,15 @@
-const url_country = 'https://restcountries.com/v2';
+const BASE_URL = `https://restcountries.com/v2`;
 
-    //забирает данные и возвращает промис
-function fetchCountriesByName(countryName) {
+function fetchCountry(countryName) {
+  if (countryName.length === 0) {
+    return Promise.resolve("");
+  }
 
-    return fetch(`${url_country}/name/${countryName}`)
-        .then(response => {
-             return response.json();  
-    })  
+  const url = `${BASE_URL}/name/${countryName}`;
+
+  return fetch(url).then((response) => {
+    return response.json();
+  });
 }
 
-export default { fetchCountriesByName };
-
-// https://restcountries.com/v2/name/{name}
-// const BASE_URL = `https://restcountries.com/v2`;
-
-// function fetchCountry(countryName) {
-//   if (countryName.length === 0) {
-//     return Promise.resolve("");
-//   }
-
-//   const url = `${BASE_URL}/name/${countryName}`;
-
-//   return fetch(url).then((response) => {
-//     return response.json();
-//   });
-// }
-
-// export default { fetchCountry };
+export default { fetchCountry };
